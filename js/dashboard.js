7fa2af51b6be3e1b2db940a62109f8241cf9fa69
areinime <areeinime@gmail.com>
2026-06-27 22:29:13 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,28 @@
+const user = JSON.parse(
 
+    localStorage.getItem(
+        "premium_user"
+    )
+
+);
+
+if(!user){
+
+    location.href =
+    "index.html";
+
+}
+
+document.getElementById(
+    "userBox"
+).innerHTML = `
+
+    <img
+    src="${user.photo}"
+    width="80">
+
+    <h2>${user.name}</h2>
+
+    <p>${user.email}</p>
+
+`;
