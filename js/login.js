4dcc131757052adc4ced5c3a0c2298aea10d7698
areinime <areeinime@gmail.com>
2026-06-27 22:21:17 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,58 @@
+import {
+    auth,
+    provider,
+    signInWithPopup
+}
+from "./firebase.js";
 
+const btn =
+document.getElementById(
+    "googleBtn"
+);
+
+btn.addEventListener(
+    "click",
+    async () => {
+
+        try {
+
+            const result =
+            await signInWithPopup(
+                auth,
+                provider
+            );
+
+            const user =
+            result.user;
+
+            localStorage.setItem(
+                "premium_user",
+                JSON.stringify({
+                    uid: user.uid,
+                    name: user.displayName,
+                    email: user.email,
+                    photo: user.photoURL
+                })
+            );
+
+            alert(
+                "Login berhasil!"
+            );
+
+            location.href =
+            "dashboard.html";
+
+        }
+
+        catch(error){
+
+            alert(
+                error.message
+            );
+
+            console.log(error);
+
+        }
+
+    }
+);
