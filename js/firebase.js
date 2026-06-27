8480125c2e4f8cfb1e6f426000af4afa5395cdea
areinime <areeinime@gmail.com>
2026-06-27 22:20:48 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,31 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup
+}
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsoXmY54RF-vuKAznFObxATSwqDfl_u68",
+    authDomain: "premium-853aa.firebaseapp.com",
+    databaseURL: "https://premium-853aa-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "premium-853aa",
+    storageBucket: "premium-853aa.firebasestorage.app",
+    messagingSenderId: "507436387791",
+    appId: "1:507436387791:web:9786e7acc83ad98c0f16ba"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export {
+    auth,
+    provider,
+    signInWithPopup
+};
